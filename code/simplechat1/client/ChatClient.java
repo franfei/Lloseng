e009c8a32a81d9410df7e32082e5b68f2fe10379
franfei <65523259+franfei@users.noreply.github.com>
2020-05-25 23:06:04 -0400

    Update ChatClient.java

--- code/simplechat1/client/ChatClient.java
+++ code/simplechat1/client/ChatClient.java
@@ -78,6 +78,18 @@ public class ChatClient extends AbstractClient
     }
   }
   
+  public void connectionClosed()
+  {
+  quit();
+  System.out.println("Server has been shut down!");
+  }
+  
+  public void connectionException()
+  {
+  System.out.println("The exception!");
+  quit();
+  }
+  
   /**
    * This method terminates the client.
    */
